c44654d7ff99ee95e4abd3d011c5d95a7d57ea18
Tertytskyi, Nikita <Nikita.Tertytskyi@gmail.com>
2018-09-03 10:47:22 +0200

    create JS project

---
+++ tests/demo.js
@@ -0,0 +1,12 @@
+module.exports = {
+    '@tags': ['demo'],
+    'Demo test Google' : function (client) {
+      client
+        .url('http://www.google.com')
+        .waitForElementVisible('body', 1000)
+        .assert.title('Google')
+        .assert.visible('input[type=text]')
+        
+        .end();
+    }
+  };
